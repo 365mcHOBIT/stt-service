03ff4e8dbb522019381998f1677ea94499eb1a8e
uxmason <uxmason@me.com>
2024-07-23 23:16:08 +0900

    ..

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false,
+    env: {
+        JWT_SECRET: "81NwD5pOYZBZmerDi6ZZG7G7VTc72Zb0",
+        ORACLE_URL: "http://20.200.186.147"
+    }
+}
+
+module.exports = nextConfig
