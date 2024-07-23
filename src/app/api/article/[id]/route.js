38a3036422d63d015cae07acebf24c0fbcd44d96
uxmason <uxmason@me.com>
2024-07-23 23:10:22 +0900

    ..

---
+++ src/app/api/article/[id]/route.js
@@ -0,0 +1,12 @@
+import sql from 'mssql';
+import { NextRequest, NextResponse  } from 'next/server';
+import ExecuteQuery from '../../../../utils/msSql';
+
+export async function GET(request, { params }) {
+  try {
+    const data2 = await ExecuteQuery(`SELECT * FROM CORE_CONSULT_SEGMENTS WHERE ARTICLE_id = ${params.id} ORDER BY END_TIME`);
+    return NextResponse.json({success:true, segments:data2}, { status: 200 });
+  } catch(e) {
+    return NextResponse.json({success:false, message:e.message}, { status: 500 });
+  }
+}
---
+++ src/app/api/consult/[id]/room/route.js
@@ -0,0 +1,13 @@
+import sql from 'mssql';
+import { NextRequest, NextResponse  } from 'next/server';
+import ExecuteQuery from '../../../../../utils/msSql';
+
+export async function PUT(request, { params }) {
+  const query = await request.json();
+  try {
+    const data2 = await ExecuteQuery(`UPDATE CORE_CONSULTS SET ROOM_id = ${query.roomID}, updatedAt = SYSDATETIME() WHERE _id = ${params.id}`);
+    return NextResponse.json({ success:true }, { status:201 });
+  } catch(e) {
+    return NextResponse.json({ success:false, message:e.message }, { status:500 });
+  }
+}
