c85e916d87bb9fdc406936bc482649cdaf3c297d
uxmason <uxmason@me.com>
2024-07-24 13:05:12 +0900

    ..

--- src/app/api/protected/route.js
+++ src/app/api/protected/route.js
@@ -1,15 +1,25 @@
 import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
-import { headers } from "next/headers";
+// import { headers } from "next/headers";
 
-export async function GET() {
+export async function GET(req) {
 
   try {
-    const headersInstance = headers();
-    const authHeader = headersInstance.get("authorization");
-
+    // const headersInstance = headers();
+    const authHeader = req.headers.get("authorization");
+    if (!authHeader) {
+      return NextResponse.json(
+        { message: "Authorization header missing" }, 
+        { status: 400 }
+      );
+    }
     const token = authHeader.split(" ")[1];
-
+    if (!token) {
+      return NextResponse.json(
+        { message: "Token missing" }, 
+        { status: 400 }
+      );
+    }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (!decoded) {
       return NextResponse.json(
