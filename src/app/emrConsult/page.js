c0a893eb215e52243f54584c822f0a7e9b96bc9a
uxmason <uxmason@me.com>
2024-08-02 21:54:44 +0900

    ..

--- src/app/emrConsult/page.js
+++ src/app/emrConsult/page.js
@@ -84,7 +84,7 @@ export default function Consult() {
     if(userID && positionID) {
       boostListData();
     }
-  }, [indexBoard, userID, positionID, sorting]);
+  }, [indexBoard, userID, positionID, sorting, dateFrom, dateTo]);
   
   useEffect(() => {
     updateDimensions();
